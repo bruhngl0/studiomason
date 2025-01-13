084a5e5f2a73e9311b0f958b1c37eedd660afbe3
bruhngl0 <adityashrm500@gmail.com>
2025-01-13 22:24:03 +0530

    proper

--- src/components/ImageAni.jsx
+++ src/components/ImageAni.jsx
@@ -8,12 +8,13 @@ const ImageAni = () => {
   return (
     <div className= "imageani-main">
         <div className='imageani-heading'>
-            <h2> MADE BY <br/>STUDIO MASON, <br/> CREATED WITH LOVE</h2>
+            <h2> CONCRETE INNOVATION, TIMELESS DESIGN
+            </h2>
         </div>
 
         <div className='imageani-para'>
             <p> Studio Mason products are made using high-performance concrete, Steel, Wood, Resin and
-            Fibreglass . <br/> <br/> Our materials offer strength, weather resistance, and a natural aesthetic appeal. Each
+            Fibreglass. <br/> <br/> Our materials offer strength, weather resistance, and a natural aesthetic appeal. Each
               piece is handcrafted and finished with attention to detail, ensuring uniqueness and longevity.
           </p>
         </div>
